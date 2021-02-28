fdef9fc174c31e5e1c4fdb06e2b9f284b9dd4501
Rodolfo Diaz <rodolfodzr@gmail.com>
2021-02-28 13:48:30 -0500

    modifiying server js for backend

--- server.js
+++ server.js
@@ -1,12 +1,39 @@
-const axios = require ("axios");
+const express = require ('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+const mongoose = require ('mongoose');
+const path = require ('path');
+const apiRouter = require ('./routes');
 
-const title= "little women"
-const ApiURL="https://www.googleapis.com/books/v1/volumes?q="
+const PORT = process.env.PORT || 3001;
 
-axios.get(ApiURL+title)
-.then(results => {
-    console.log(results.data);
-})
-.catch((err)=>{
-    console.log(err);
-})
+app.use(cookieParser());
+app.use(express.json());
+app.use(apiRouter);
+
+// configuring AtlasDB connection for Heroku Deployment 
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/movieapp",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false   
+    },
+    () =>{ console.log("Successfully connected to DataBase")}
+);
+
+  // preparing production environment for heroku deployment 
+  if(process.env.NODE_ENV === 'production'){
+    // this calls the build folder to our server 
+    app.use(express.static('client/build'));
+      // this will server the index.html from the front end 
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
