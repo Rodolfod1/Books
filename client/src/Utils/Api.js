9cbdbbd6337c5d8bda8957296690d1a4db7ae777
Rodolfo Diaz <rodolfodzr@gmail.com>
2021-03-01 00:01:49 -0500

    connecting back to front end

--- client/src/Utils/Api.js
+++ client/src/Utils/Api.js
@@ -3,9 +3,20 @@ import axios from "axios";
 const Api = () => {
     getBook = (BookTitle) =>{
         //call to backend routes 
-        return axios.get("/api/results/" + BookTitle)
+        return axios.get("/api/gApi/" + BookTitle)
+    },
+    //get all stored books on the database 
+    getSavedBooks = () =>{
+        return axios.get("/api/Books")
+    },
+    //saves books on the database 
+    saveBook = (BookData) =>{
+        return axios.post("/api/Books", BookData);
+    },
+    // removes book from database 
+    deleteBook = (id) =>{
+        return axios.delete("/api/books/" + id)
     }
- 
-}
+ };
 
 export default Api
