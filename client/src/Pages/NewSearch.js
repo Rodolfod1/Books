b9296b3978dbcd0899e26e778ba470941267d05b
Rodolfo Diaz <rodolfodzr@gmail.com>
2021-03-03 00:57:49 -0500

    adding mapping functions to the arrays

--- client/src/Pages/NewSearch.js
+++ client/src/Pages/NewSearch.js
@@ -27,7 +27,7 @@ const SearchBook = a =>{
     console.log(BookTitle);
     Api.getBook(BookTitle)
         .then(res => 
-            setSingleBook(res.data.items))
+            setSingleBook(res.data))
             .catch(err => console.log(err))
 
 }
@@ -35,7 +35,46 @@ console.log(singleBook);
 
     return (
         <div>
-            <SearchItem SearchBook={SearchBook} />      
+            <SearchItem SearchBook={SearchBook} />  
+
+
+       
+
+        <div>
+        <div className="card" style={{width: 18+ "rem"}}>
+  <img src="..." className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>   
+            {singleBook.items !==undefined && singleBook.items[0].volumeInfo.description !==undefined ? (
+                singleBook.items.map(({volumeInfo,id,searchInfo}) => {
+                    return(
+                            console.log(volumeInfo.description)
+                            
+
+
+
+
+                    
+                        // <h1>{volumeInfo.title}</h1>
+                        // <h1>{volumeInfo.authors}</h1>
+                        // <h1>{volumeInfo.categories}</h1>
+                        // <h1>{volumeInfo.description}</h1>
+                        
+                    )
+                })
+            ):
+            ( <></>)
+        }    
+
+
+
+                
+            </div>
+      
             
         </div>
     )
