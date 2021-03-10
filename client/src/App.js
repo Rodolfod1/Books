4c4689c8ad1ff8d518843ab7a2e1e31fef0fc93f
Rodolfo Diaz <rodolfodzr@gmail.com>
2021-03-10 18:01:21 -0500

    deploying build

--- client/src/App.js
+++ client/src/App.js
@@ -11,26 +11,26 @@ import Footer from "./Components/Footer"
 
 function App() {
   return (
-      // <Router>
-      //     <Hero />
-      //   <div className="App">
-      //      <Switch>
-      //       <Route exact path="/">
-      //         <Home />
-      //       </Route>
-      //       <Route exact path="/Library">
-      //         <Library/>
-      //       </Route>
-      //       <Route  exact path="/Search">
-      //         <NewSearch />
-      //       </Route>
-      //     </Switch>
-      //   </div>
-      //   <Footer />
-      // </Router>
- <div>
- <Home />
- </div> 
+    <div className="App">
+      <Hero />
+      <Router>
+       
+        
+           <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/Library">
+              <Library/>
+            </Route>
+            <Route  exact path="/Search">
+              <NewSearch />
+            </Route>
+          </Switch>
+       
+        <Footer />
+      </Router>
+      </div>
        
 
   );
--- server.js
+++ server.js
@@ -1,18 +1,34 @@
 const express = require ('express');
-const app = express();
-const cookieParser = require('cookie-parser');
+
+// const cookieParser = require('cookie-parser');
 const mongoose = require ('mongoose');
 const path = require ('path');
 const apiRouter = require ('./routes');
+const app = express();
 
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
-app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(apiRouter);
 
-// app.use(express.urlencoded({ extended: true }));
+
+  // preparing production environment for heroku deployment 
+  if(process.env.NODE_ENV === 'production'){
+    // this calls the build folder to our server 
+    app.use(express.static('client/build'));
+      // this will server the index.html from the front end 
+    // app.get('*',(req,res)=>{
+    //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  
+    // })
+}
+
+
+
+// app.use(cookieParser());
+
+app.use(apiRouter);
 
 
 
@@ -28,18 +44,6 @@ mongoose.connect(
     },
     () =>{ console.log("Successfully connected to DataBase")}
 );
-
-  // preparing production environment for heroku deployment 
-  if(process.env.NODE_ENV === 'production'){
-    // this calls the build folder to our server 
-    app.use(express.static('client/build'));
-      // this will server the index.html from the front end 
-    // app.get('*',(req,res)=>{
-    //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-        app.get('/*',(req,res)=>{
-          res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
-    })
-}
 
 
 app.listen(PORT, () => {
